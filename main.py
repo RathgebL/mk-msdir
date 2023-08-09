46be40e11267acfed1de242e4776de1837263dba
RathgebL <108350061+RathgebL@users.noreply.github.com>
2023-08-09 10:32:12 +0200

    feat: Add middle name

--- main.py
+++ main.py
@@ -7,19 +7,24 @@ frame = tkinter.Frame(window)
 frame.pack()
 
 #Saving user information
-user_info_frame = tkinter.LableFrame(frame, text = "Composer")
-user_info_frame.grid(row = 0, column = 0, padx = 20, pady = 20)
+composer_frame = tkinter.LableFrame(frame, text = "Composer")
+composer_frame.grid(row = 0, column = 0, padx = 20, pady = 20)
 
-first_name_label = tkinter.label(user_info_frame, text = "First Name")
+first_name_label = tkinter.label(composer_frame, text = "First Name")
 first_name_label.grid(row = 0, column = 0)
 
-last_name_label = tkinter.label(user_info_frame, text = "Last Name")
-last_name_label.grid(row = 0, column = 1)
+middle_name_label = tkinter.label(composer_frame, text = "Middle Name")
+middle_name_label.grid(row = 0, column = 1)
 
-first_name_entry = tkinter.Entry(user_info_frame)
-last_name_entry = tkinter.Entry(user_info_frame)
+last_name_label = tkinter.label(composer_frame, text = "Last Name")
+last_name_label.grid(row = 0, column = 2)
+
+first_name_entry = tkinter.Entry(composer_frame)
+middle_name_entry = tkinter.Entry(composer_frame)
+last_name_entry = tkinter.Entry(composer_frame)
 first_name_entry.grid(row = 1, column = 0)
-first_name_entry.grid(row = 1, column = 1)
+middle_name_entry.grid(row = 1, column = 1)
+last_name_entry.grid(row = 1, column = 2)
  
 
 window.mainloop()
